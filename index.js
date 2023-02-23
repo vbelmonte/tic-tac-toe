e4a5bc423954862a3eb2f7eabde43be9a752dd4a
Villy Belmonte <vjbbelmont@gmail.com>
2023-02-22 20:38:52 -0800

    Add function for Start button on landing screen

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("start").addEventListener("click", start);
+
+function start() {
+    document.getElementById("start-window").style.display = "none";
+    document.getElementById("select-mode").style.display = "flex";
+}
