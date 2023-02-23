f934cade4f0c45c749a59cd37fa0976adcf1cde2
Villy Belmonte <vjbbelmont@gmail.com>
2023-02-22 21:09:18 -0800

    Rewrote start and mode selection functions into factory functions

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-document.getElementById("start").addEventListener("click", start);
-
-function start() {
-    document.getElementById("start-window").style.display = "none";
-    document.getElementById("select-mode").style.display = "flex";
+function runProgram() {
+    function backEndGameMechanics() {
+        function start() {
+            document.getElementById("start-window").style.display = "none";
+            document.getElementById("select-mode").style.display = "flex";
+        }
+    
+        function startTwoPlayers() {
+            document.getElementById("select-mode").style.display = "none";
+            document.getElementsByClassName("landing-screen")[0].style.display = "none";
+            document.getElementsByClassName("main-screen")[0].style.display = "flex";
+        }
+    
+        return {start, startTwoPlayers};
+    }
+    
+    const gameOperation = backEndGameMechanics();
+    
+    document.getElementById("start").addEventListener("click", gameOperation.start);
+    document.getElementById("two-players").addEventListener("click", gameOperation.startTwoPlayers);
 }
+
+runProgram();
