6671fa11976483815df433c877203513f8c93dca
Villy Belmonte <vjbbelmont@gmail.com>
2023-02-23 18:32:43 -0800

    Implement info modal functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function runProgram() {
     function backEndGameMechanics() {
+        
         function start() {
             document.getElementById("start-window").style.display = "none";
             document.getElementById("select-mode").style.display = "flex";
@@ -10,14 +11,27 @@ function runProgram() {
             document.getElementsByClassName("landing-screen")[0].style.display = "none";
             document.getElementsByClassName("main-screen")[0].style.display = "flex";
         }
+
+        function toggleInfoModal() {
+            let infoModal = document.getElementById("modal-info");
+
+            if (infoModal.style.display === "none") {
+                infoModal.style.display = "flex";
+            }
+            else {
+                infoModal.style.display = "none";
+            }
+        }
     
-        return {start, startTwoPlayers};
+        return {start, startTwoPlayers, toggleInfoModal};
     }
     
     const gameOperation = backEndGameMechanics();
     
     document.getElementById("start").addEventListener("click", gameOperation.start);
     document.getElementById("two-players").addEventListener("click", gameOperation.startTwoPlayers);
+    document.getElementById("info").addEventListener("click", gameOperation.toggleInfoModal);
+    document.getElementsByClassName("btn-x")[0].addEventListener("click", gameOperation.toggleInfoModal);
 }
 
 runProgram();
